902d0e919bd452543e50b9910c26b2873256f941
antelopest <antelopest@gmail.com>
2025-07-14 14:34:21 +0700

    main setting vite and change html, css

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    css: {
-        postcss: './postcss.config.js'
-    }
+  base: '/stats-preview-card-component/',
+  server: {
+    port: 3000,
+    host: true
+  },
+  css: {
+    postcss: './postcss.config.js'
+  }
 });
